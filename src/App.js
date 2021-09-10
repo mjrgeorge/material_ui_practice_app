f01d42be6561803371f9a5c059c01110bd9a5323
mjrgeorge <mjrgeorge@gmail.com>
2021-09-10 21:45:31 +0600

    apps updated

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './style.css';
-import About from './components/About';
-import Extra from './components/Extra';
-import Home from './components/Home';
-import MyBreadcrumb from './components/MyBreadcrumb';
-import MyComponent from './components/MyComponent';
-import MyMenuBar from './components/myMenuBar/MyMenuBar';
-import mySidebar from './components/mySidebar';
-import MyVideoPlayer from './components/myVideoPlayer/MyVideoPlayer';
-import MyReactVideoPlayer from './components/myReactVideoPlayer/MyReactVideoPlayer';
-import VideoSidebar from './components/videoSidebar/VideoSidebar';
+import { Button, ButtonGroup } from "@material-ui/core";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import AudioRecorder from './components/audioRecorder/AudioRecorder';
+import Home from './components/Home';
 import PageAudioRecorder from './components/myAudioRecorder/PageAudioRecorder';
+import './style.css';
 
 function App() {
   return (
     <Router>
-      <MyBreadcrumb />
-      <Switch>
-        <Route exact path="/"component={Home} />
-        <Route exact path="/pageAudioRecorder"component={PageAudioRecorder} />
-        <Route exact path="/audioRecorder"component={AudioRecorder} />
-        <Route exact path="/myVideoPlayer"component={MyVideoPlayer} />
-        <Route exact path="/myComponent"component={MyComponent} />
-        <Route exact path="/myReactVideoPlayer"component={MyReactVideoPlayer} />
-        <Route exact path="/videoSidebar"component={VideoSidebar} />
-        <Route exact path="/about"component={About} />
-        <Route exact path="/extra"component={Extra} />
-        <Route exact path="/myMenuBar"component={MyMenuBar} />
-        <Route exact path="/mySidebar"component={mySidebar} />
-      </Switch>
+      <div>
+        <ButtonGroup color="primary" >
+          <Button>
+            <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
+          </Button>
+          <Button>
+            <Link to="/audioRecorder" style={{ textDecoration: 'none' }}>Audio Recorder</Link>
+          </Button>
+          <Button>
+            <Link to="/pageAudioRecorder" style={{ textDecoration: 'none' }}>Page Audio Recorder</Link>
+          </Button>
+        </ButtonGroup>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/audioRecorder">
+            <AudioRecorder />
+          </Route>
+          <Route exact path="/pageAudioRecorder">
+            <PageAudioRecorder />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
