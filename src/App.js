5037e1f939bcd8aa18bc9aeaa839970ce6b6836c
mjrgeorge <mjrgeorge@gmail.com>
2021-09-11 23:16:32 +0600

    rollbar added

--- src/App.js
+++ src/App.js
@@ -10,35 +10,43 @@ import AudioRecorder from './components/audioRecorder/AudioRecorder';
 import Home from './components/Home';
 import PageAudioRecorder from './components/myAudioRecorder/PageAudioRecorder';
 import './style.css';
+import { Provider, ErrorBoundary } from '@rollbar/react'; // <-- Provider imports 'rollbar' for us
 
 function App() {
+  const rollbarConfig = {
+    accessToken: "968cb10b639e40a1b4b216bbb320280c",
+    environment: 'production',
+  };
+
   return (
-    <Router>
-      <div>
-        <ButtonGroup color="primary" >
-          <Button>
-            <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
-          </Button>
-          <Button>
-            <Link to="/audioRecorder" style={{ textDecoration: 'none' }}>Audio Recorder</Link>
-          </Button>
-          <Button>
-            <Link to="/pageAudioRecorder" style={{ textDecoration: 'none' }}>Page Audio Recorder</Link>
-          </Button>
-        </ButtonGroup>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/audioRecorder">
-            <AudioRecorder />
-          </Route>
-          <Route exact path="/pageAudioRecorder">
-            <PageAudioRecorder />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider config={rollbarConfig}>
+      <Router>
+        <div>
+          <ButtonGroup color="primary" >
+            <Button>
+              <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
+            </Button>
+            <Button>
+              <Link to="/audioRecorder" style={{ textDecoration: 'none' }}>Audio Recorder</Link>
+            </Button>
+            <Button>
+              <Link to="/pageAudioRecorder" style={{ textDecoration: 'none' }}>Page Audio Recorder</Link>
+            </Button>
+          </ButtonGroup>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/audioRecorder">
+              <AudioRecorder />
+            </Route>
+            <Route exact path="/pageAudioRecorder">
+              <PageAudioRecorder />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
+
+    function UserException(message) {
+        this.message = message;
+        this.name = 'UserException';
+    }
+    function getMonthName(mo) {
+        mo = mo - 1; // Adjust month number for array index (1 = Jan, 12 = Dec)
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+            'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        if (months[mo] !== undefined) {
+            return months[mo];
+        } else {
+            throw new UserException('InvalidMonthNo');
+        }
+    }
+
+    try {
+        // statements to try
+        var myMonth = 15; // 15 is out of bound to raise the exception
+        var monthName = getMonthName(myMonth);
+    } catch (e) {
+        monthName = 'unknown';
+        console.error(e.message, e.name); // pass exception object to err handler
+    }
+
     return (
         <div>
-            <h1>This is home page!</h1>
+            <h1>Home page!</h1>
         </div>
     )
 }
